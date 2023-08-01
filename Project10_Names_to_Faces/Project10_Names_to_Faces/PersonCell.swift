7452a2bd71083ac9e5a97d7e16eed41dfffdc459
Petro Strynada <petro.strynada@gmail.com>
2023-08-01 12:49:53 +0300

    Setting up
    + project
    + gitignore
    + readme

---
+++ Project10_Names_to_Faces/Project10_Names_to_Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10_Names_to_Faces
+//
+//  Created by Petro Strynada on 21.07.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
