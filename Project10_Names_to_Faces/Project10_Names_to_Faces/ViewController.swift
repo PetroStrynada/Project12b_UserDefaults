4b7c268ca75d964fba39892277743a3bca004bb1
Petro Strynada <petro.strynada@gmail.com>
2023-08-02 17:12:33 +0300

    Fixing Project 10: Codable
    + Codable to ecode and encode information. Now we can save and load data
    with chosen pictures

--- Project10_Names_to_Faces/Project10_Names_to_Faces/ViewController.swift
+++ Project10_Names_to_Faces/Project10_Names_to_Faces/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(addNewPersonWithCamera))
+
+        load()
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -71,6 +73,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
 
         dismiss(animated: true)
@@ -110,6 +113,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
 
@@ -121,6 +125,31 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         if let index = people.firstIndex(of: person) {
             people.remove(at: index)
             collectionView.reloadData()
+        }
+    }
+
+    func save() {
+        let jsonEncoder = JSONEncoder()
+
+        if let saveData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(saveData, forKey: "people")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+
+    func load() {
+        let defaults = UserDefaults.standard
+
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
         }
     }
 
